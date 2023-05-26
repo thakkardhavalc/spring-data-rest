a4eaaffaa5d2adf4abc7ab49b4fde6a35887b306
thakkardhavalc <thakkardhavalc@gmail.com>
2023-05-26 17:28:53 -0600

    added @Builder annotation and implemented implementation of service

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/service/BeerServiceImpl.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/service/BeerServiceImpl.java
@@ -1,12 +1,24 @@
 package guru.springframework.spring6restmvc.service;
 
 import guru.springframework.spring6restmvc.model.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
-        return null;
+        return Beer.builder()
+                .id(id)
+                .version(1)
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("123456")
+                .price(new BigDecimal("12.99"))
+                .createdDate(LocalDateTime.now())
+                .updatedDate(LocalDateTime.now())
+                .build();
     }
 }

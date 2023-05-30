145f7bb6859d610b4d11e6188c0b70a3515fb595
thakkardhavalc <thakkardhavalc@gmail.com>
2023-05-30 10:00:45 -0600

    added @PostMapping to create a new record in database

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -4,8 +4,12 @@ import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +35,13 @@ public class BeerController {
         log.debug("Get Beer by Id - in controller");
 
         return beerService.getBeerById(beerId);
+    }
+
+    @PostMapping("create")
+    public ResponseEntity handlePost(@RequestBody Beer beer) {
+
+        Beer savedBeer = beerService.saveNewBeer(beer);
+
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }

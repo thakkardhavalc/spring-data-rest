8450ed366d40ee90c4843f4fba3905b57d31055a
thakkardhavalc <thakkardhavalc@gmail.com>
2023-05-31 16:02:40 -0600

    added jpa annotation and used @Id annotation to mark id variable

---
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
@@ -0,0 +1,38 @@
+package guru.springframework.spring6restmvc.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    private String customerName;
+
+    @Version
+    private Integer version;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}

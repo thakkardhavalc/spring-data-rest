350555ca701a83a73ab06750b9face5c76b74ea5
thakkardhavalc <thakkardhavalc@gmail.com>
2023-06-01 11:24:36 -0600

    mapped customerDto to customerJpa and added test cases to check the assertion

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/service/CustomerServiceJPA.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/service/CustomerServiceJPA.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Primary
@@ -21,12 +22,17 @@ public class CustomerServiceJPA implements CustomerService {
 
     @Override
     public List<CustomerDTO> listCustomers() {
-        return null;
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Optional<CustomerDTO> getCustomerById(UUID id) {
-        return Optional.empty();
+        return Optional.ofNullable(customerMapper
+                .customerToCustomerDto(customerRepository.findById(id)
+                        .orElse(null)));
     }
 
     @Override

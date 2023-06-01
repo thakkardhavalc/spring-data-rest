4bf54468fe31ef23c5d69c6c1b1a90b103d72c3b
thakkardhavalc <thakkardhavalc@gmail.com>
2023-06-01 17:46:59 -0600

    added validation on beerName and also annotated method with @validated

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -44,7 +45,7 @@ public class BeerController {
     }
 
     @PostMapping(BEER_PATH)
-    public ResponseEntity handlePost(@RequestBody BeerDTO beer) {
+    public ResponseEntity handlePost(@Validated @RequestBody BeerDTO beer) {
 
         BeerDTO savedBeer = beerService.saveNewBeer(beer);
 

cd768c90a735ce72111171dc79920a3b8a5e8ab1
thakkardhavalc <thakkardhavalc@gmail.com>
2023-06-01 10:43:56 -0600

    created data for in memory database set up bootstrap class and added those data to validate that I've added test cases to support the insertions.

---
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/bootstrap/BootstrapData.java
@@ -0,0 +1,93 @@
+package guru.springframework.spring6restmvc.bootstrap;
+
+import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.entities.Customer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
+import guru.springframework.spring6restmvc.repositories.BeerRepository;
+import guru.springframework.spring6restmvc.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BootstrapData implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        loadBeerData();
+        loadCustomerData();
+    }
+
+    private void loadBeerData() {
+
+        if (beerRepository.count() == 0) {
+            Beer beer1 = Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("123456")
+                    .price(new BigDecimal("12.99"))
+                    .quantityOnHand(122)
+                    .createdDate(LocalDateTime.now())
+                    .updatedDate(LocalDateTime.now())
+                    .build();
+
+            Beer beer2 = Beer.builder()
+                    .beerName("Crank")
+                    .beerStyle(BeerStyle.GOSE)
+                    .upc("123477")
+                    .price(new BigDecimal("13.99"))
+                    .quantityOnHand(392)
+                    .createdDate(LocalDateTime.now())
+                    .updatedDate(LocalDateTime.now())
+                    .build();
+
+            Beer beer3 = Beer.builder()
+                    .beerName("Sunshine city")
+                    .beerStyle(BeerStyle.IPA)
+                    .upc("123488")
+                    .price(new BigDecimal("14.99"))
+                    .quantityOnHand(595)
+                    .createdDate(LocalDateTime.now())
+                    .updatedDate(LocalDateTime.now())
+                    .build();
+
+            beerRepository.saveAll(Arrays.asList(beer1, beer2, beer3));
+        }
+    }
+
+    private void loadCustomerData() {
+
+        if (customerRepository.count() == 0) {
+            Customer customer1 = Customer.builder()
+                    .customerName("John Harvey")
+                    .createdDate(LocalDateTime.now())
+                    .lastModifiedDate(LocalDateTime.now())
+                    .build();
+
+            Customer customer2 = Customer.builder()
+                    .customerName("Ricky Harvey")
+                    .createdDate(LocalDateTime.now())
+                    .lastModifiedDate(LocalDateTime.now())
+                    .build();
+
+            Customer customer3 = Customer.builder()
+                    .customerName("Adam Harvey")
+                    .createdDate(LocalDateTime.now())
+                    .lastModifiedDate(LocalDateTime.now())
+                    .build();
+
+            customerRepository.saveAll(Arrays.asList(customer1, customer2, customer3));
+        }
+    }
+}

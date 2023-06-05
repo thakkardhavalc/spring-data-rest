713ccfb8b77bcfd3921b140bebe1515195c280da
thakkardhavalc <thakkardhavalc@gmail.com>
2023-06-05 10:40:42 -0600

    added validation to the database side as well into entity class

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Version;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -33,10 +35,20 @@ public class Beer {
 
     @Version
     private Integer version;
+
+    @NotNull
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyle beerStyle;
+
+    @NotNull
+    @NotBlank
     private String upc;
     private Integer quantityOnHand;
+
+    @NotNull
     private BigDecimal price;
     private LocalDateTime createdDate;
     private LocalDateTime updatedDate;

8b1894f7acf2f8162fde349d0c4255573707c4fa
thakkardhavalc <thakkardhavalc@gmail.com>
2023-06-08 11:05:47 -0600

    created new method into bootstrap and added data into database, also fixed unit test case

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/service/BeerCsvServiceImpl.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/service/BeerCsvServiceImpl.java
@@ -2,12 +2,14 @@ package guru.springframework.spring6restmvc.service;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import guru.springframework.spring6restmvc.model.BeerCSVRecord;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
+@Service
 public class BeerCsvServiceImpl implements BeerCsvService {
     @Override
     public List<BeerCSVRecord> convertCSV(File csvFile) {

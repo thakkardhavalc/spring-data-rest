e16c36e6dc41d9a92c007bf21a282d18bf8310d2
thakkardhavalc <thakkardhavalc@gmail.com>
2023-06-08 11:13:21 -0600

    updated null createdDate and updatedDate into database

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -14,8 +14,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
@@ -57,6 +59,10 @@ public class Beer {
 
     @NotNull
     private BigDecimal price;
+
+    @CreationTimestamp
     private LocalDateTime createdDate;
+
+    @UpdateTimestamp
     private LocalDateTime updatedDate;
 }

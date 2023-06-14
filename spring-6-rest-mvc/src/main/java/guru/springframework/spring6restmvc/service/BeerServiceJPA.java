16a2e6e45a43135e81c00cc0ac836c82d2c25bc8
thakkardhavalc <thakkardhavalc@gmail.com>
2023-06-14 12:58:48 -0600

    added two properties and set the value for that

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/service/BeerServiceJPA.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/service/BeerServiceJPA.java
@@ -113,6 +113,8 @@ public class BeerServiceJPA implements BeerService {
             foundBeer.setBeerStyle(beer.getBeerStyle());
             foundBeer.setUpc(beer.getUpc());
             foundBeer.setPrice(beer.getPrice());
+            foundBeer.setQuantityOnHand(beer.getQuantityOnHand());
+            foundBeer.setVersion(beer.getVersion());
             atomicReference.set(Optional.of(beerMapper
                     .beerToBeerDto(beerRepository.save(foundBeer))));
         }, () -> {

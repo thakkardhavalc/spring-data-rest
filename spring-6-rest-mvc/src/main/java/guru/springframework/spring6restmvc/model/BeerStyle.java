4ca76496dedbb524c7bc7fc2f94280feaae2dfea
thakkardhavalc <88214748+thakkardhavalc@users.noreply.github.com>
2023-06-14 13:17:06 -0600

    Delete spring-6-rest-mvc directory

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/model/BeerStyle.java
+++
@@ -1,5 +0,0 @@
-package guru.springframework.spring6restmvc.model;
-
-public enum BeerStyle {
-    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
-}
--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/repositories/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package guru.springframework.spring6restmvc.repositories;
-
-import guru.springframework.spring6restmvc.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CustomerRepository extends JpaRepository<Customer, UUID> {
-}
